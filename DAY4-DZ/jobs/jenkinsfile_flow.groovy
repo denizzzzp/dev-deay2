d77d00e78ba677fc0da575cd7a8cc15ce5859482
root <root@devops1>
2019-06-26 23:45:14 +0300

    Создание своего пайплайна Jenkinsfile fix#1

--- DAY4-DZ/jobs/jenkinsfile_flow.groovy
+++ DAY4-DZ/jobs/jenkinsfile_flow.groovy
@@ -19,7 +19,7 @@ pipelineJob("my-first-pipeline") {
                   }
               }
         }
-        scriptPath("jenkins/Jenkinsfile")
+        scriptPath("DAY4-DZ/jenkins/Jenkinsfile")
       }
 
     }
